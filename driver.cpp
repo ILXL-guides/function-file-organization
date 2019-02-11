704e120b8afe8581acc8687ca594eafa31402b93
Paul Inventado <pinventado@gmail.com>
2019-02-11 08:12:20 -0800

    use std::

--- driver.cpp
+++ driver.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
 #include "algebra.hpp"
-using namespace std;
 
 int main() {
-  cout << "The cube of 5 is " << cube(5) << endl;
-  cout << "The cube of 3 is " << cube(3) << endl;
-  cout << "The cube of 9 is " << cube(9) << endl;
+  std::cout << "The cube of 5 is " << cube(5) << "\n";
+  std::cout << "The cube of 3 is " << cube(3) << "\n";
+  std::cout << "The cube of 9 is " << cube(9) << "\n";
   return 0;
 }
