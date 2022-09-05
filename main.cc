e788d10dfea90e43777e4f727357f8bd5b57bd71
Paul Inventado <pinventado@fullerton.edu>
2022-09-05 11:12:16 -0700

    Apply Google C++ style guide

--- main.cc
+++ main.cc
@@ -2,8 +2,8 @@
 #include "algebra.h"
 
 int main() {
-  std::cout << "The cube of 5 is " << cube(5) << "\n";
-  std::cout << "The cube of 3 is " << cube(3) << "\n";
-  std::cout << "The cube of 9 is " << cube(9) << "\n";
+  std::cout << "The cube of 5 is " << Cube(5) << "\n";
+  std::cout << "The cube of 3 is " << Cube(3) << "\n";
+  std::cout << "The cube of 9 is " << Cube(9) << "\n";
   return 0;
 }
